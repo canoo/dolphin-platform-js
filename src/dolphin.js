f0983371fe67f005dfe185f4c9725db9c5210385
Michael Heinrichs <michael.heinrichs@netopyr.com>
2015-05-12 06:53:58 +0200

    Moved local functions outside of visible class

--- src/dolphin.js
+++ src/dolphin.js
@@ -22,6 +22,78 @@ var DOLPHIN_LIST_DEL_FROM_CLIENT = '@@@ LIST_DEL_FROM_CLIENT @@@';
 var DOLPHIN_LIST_SET_FROM_CLIENT = '@@@ LIST_SET_FROM_CLIENT @@@';
 
 
+function onModelAdded(dolphin, model) {
+    var type = model.presentationModelType;
+    switch (type) {
+        case DOLPHIN_BEAN:
+            dolphin.classRepository.registerClass(model);
+            break;
+        case DOLPHIN_LIST_ADD_FROM_SERVER:
+            dolphin.classRepository.addListEntry(model);
+            dolphin.dolphin.getClientModelStore().deletePresentationModel(model);
+            break;
+        case DOLPHIN_LIST_DEL_FROM_SERVER:
+            dolphin.classRepository.delListEntry(model);
+            dolphin.dolphin.getClientModelStore().deletePresentationModel(model);
+            break;
+        case DOLPHIN_LIST_SET_FROM_SERVER:
+            dolphin.classRepository.setListEntry(model);
+            dolphin.dolphin.getClientModelStore().deletePresentationModel(model);
+            break;
+        case DOLPHIN_LIST_ADD_FROM_CLIENT:
+        case DOLPHIN_LIST_DEL_FROM_CLIENT:
+        case DOLPHIN_LIST_SET_FROM_CLIENT:
+            // do nothing
+            break;
+        default:
+            var bean = dolphin.classRepository.load(model);
+            var handlerList = dolphin.addedHandlers.get(type);
+            if (exists(handlerList)) {
+                handlerList.forEach(function(handler) {
+                    handler(bean);
+                });
+            }
+            dolphin.allAddedHandlers.forEach(function(handler) {
+                handler(bean);
+            });
+            break;
+    }
+}
+
+function onModelRemoved(dolphin, model) {
+    var type = model.presentationModelType;
+    switch (type) {
+        case DOLPHIN_BEAN:
+            dolphin.classRepository.unregisterClass(model);
+            break;
+        case DOLPHIN_LIST_ADD_FROM_SERVER:
+        case DOLPHIN_LIST_DEL_FROM_SERVER:
+        case DOLPHIN_LIST_SET_FROM_SERVER:
+        case DOLPHIN_LIST_ADD_FROM_CLIENT:
+        case DOLPHIN_LIST_DEL_FROM_CLIENT:
+        case DOLPHIN_LIST_SET_FROM_CLIENT:
+            // do nothing
+            break;
+        default:
+            var bean = dolphin.classRepository.unload(model);
+            if (exists(bean)) {
+                var handlerList = dolphin.removedHandlers.get(type);
+                if (exists(handlerList)) {
+                    handlerList.forEach(function(handler) {
+                        handler(bean);
+                    });
+                }
+                dolphin.allRemovedHandlers.forEach(function(handler) {
+                    handler(bean);
+                });
+            }
+            break;
+    }
+
+}
+
+
+
 function Dolphin(url, config) {
     var _this = this;
     this.dolphin = opendolphin.dolphin(url, true, 4);
@@ -36,82 +108,12 @@ function Dolphin(url, config) {
     this.allAddedHandlers = [];
     this.allRemovedHandlers = [];
 
-    function onModelAdded(model) {
-        var type = model.presentationModelType;
-        switch (type) {
-            case DOLPHIN_BEAN:
-                _this.classRepository.registerClass(model);
-                break;
-            case DOLPHIN_LIST_ADD_FROM_SERVER:
-                _this.classRepository.addListEntry(model);
-                _this.dolphin.getClientModelStore().deletePresentationModel(model);
-                break;
-            case DOLPHIN_LIST_DEL_FROM_SERVER:
-                _this.classRepository.delListEntry(model);
-                _this.dolphin.getClientModelStore().deletePresentationModel(model);
-                break;
-            case DOLPHIN_LIST_SET_FROM_SERVER:
-                _this.classRepository.setListEntry(model);
-                _this.dolphin.getClientModelStore().deletePresentationModel(model);
-                break;
-            case DOLPHIN_LIST_ADD_FROM_CLIENT:
-            case DOLPHIN_LIST_DEL_FROM_CLIENT:
-            case DOLPHIN_LIST_SET_FROM_CLIENT:
-                // do nothing
-                break;
-            default:
-                var bean = _this.classRepository.load(model);
-                var handlerList = _this.addedHandlers.get(type);
-                if (exists(handlerList)) {
-                    handlerList.forEach(function(handler) {
-                        handler(bean);
-                    });
-                }
-                _this.allAddedHandlers.forEach(function(handler) {
-                    handler(bean);
-                });
-                break;
-        }
-    }
-
-    function onModelRemoved(model) {
-        var type = model.presentationModelType;
-        switch (type) {
-            case DOLPHIN_BEAN:
-                _this.classRepository.unregisterClass(model);
-                break;
-            case DOLPHIN_LIST_ADD_FROM_SERVER:
-            case DOLPHIN_LIST_DEL_FROM_SERVER:
-            case DOLPHIN_LIST_SET_FROM_SERVER:
-            case DOLPHIN_LIST_ADD_FROM_CLIENT:
-            case DOLPHIN_LIST_DEL_FROM_CLIENT:
-            case DOLPHIN_LIST_SET_FROM_CLIENT:
-                // do nothing
-                break;
-            default:
-                var bean = _this.classRepository.unload(model);
-                if (exists(bean)) {
-                    var handlerList = _this.removedHandlers.get(type);
-                    if (exists(handlerList)) {
-                        handlerList.forEach(function(handler) {
-                            handler(bean);
-                        });
-                    }
-                    _this.allRemovedHandlers.forEach(function(handler) {
-                        handler(bean);
-                    });
-                }
-                break;
-        }
-
-    }
-
     this.dolphin.getClientModelStore().onModelStoreChange(function (event) {
         var model = event.clientPresentationModel;
         if (event.eventType === opendolphin.Type.ADDED) {
-            onModelAdded(model);
+            onModelAdded(_this, model);
         } else if (event.eventType === opendolphin.Type.REMOVED) {
-            onModelRemoved(model);
+            onModelRemoved(_this, model);
         }
     });
 }
