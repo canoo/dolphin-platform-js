6c358a3a77c025586ef08b254bb01b7dbe12f1d6
Michael Heinrichs <michael.heinrichs@netopyr.com>
2015-06-05 22:12:19 +0200

    test-dolphin completely migrated

--- test/src/test-dolphin.js
+++ test/src/test-dolphin.js
@@ -17,9 +17,7 @@ describe('Dolphin Message Distribution', function() {
     beforeEach(sinon.test(function() {
         this.stub(opendolphin, "dolphin").returns(
             {
-                getClientModelStore: function() {
-                    return clientModelStore;
-                }
+                getClientModelStore: function() { return clientModelStore; }
             }
         );
         dolphin = connect("http://localhost");
@@ -112,179 +110,155 @@ describe('Dolphin Message Distribution', function() {
 
 
 
-//describe('Dolphin Event Handling', function() {
-//
-//    var onModelStoreChange = null;
-//    var dolphin = null;
-//
-//    beforeEach(function() {
-//        var clientModelStore = { onModelStoreChange: function(cb) { onModelStoreChange = cb; } };
-//        var opendolphin = { getClientModelStore: function() { return clientModelStore; } };
-//        var opendolphinStub = {
-//            dolphin: function() { return opendolphin },
-//            Type: { ADDED: "ADDED", REMOVED: "REMOVED" },
-//            '@noCallThru': true
-//        };
-//        var classrepoStub = {
-//            ClassRepository: function () { },
-//            '@noCallThru': true
-//        };
-//
-//        var dolphinjs = proxyquire('../src/dolphin.js', { 'opendolphin': opendolphinStub, './classrepo.js': classrepoStub });
-//        var connect = dolphinjs.connect;
-//        dolphin = connect("http://localhost");
-//        if (typeof onModelStoreChange !== 'function') {
-//            throw new Error('Initialisation of opendolphin failed');
-//        }
-//    });
-//
-//    afterEach(function() {
-//        dolphin.shutdown();
-//    });
-//
-//
-//
-//    it('should call onAdded-handler for class', sinon.test(function() {
-//        var bean = {};
-//        dolphin.classRepository.load = this.stub().returns(bean);
-//        var model = { presentationModelType: 'SomeClass' };
-//        var onAddedHandler = this.spy();
-//
-//        dolphin.onAdded('SomeClass', onAddedHandler);
-//        onModelStoreChange({ clientPresentationModel: model, eventType: "ADDED" });
-//
-//        sinon.assert.calledWith(onAddedHandler, bean);
-//    }));
-//
-//
-//    it('should not call onAdded-handler for other class', sinon.test(function() {
-//        var bean = {};
-//        dolphin.classRepository.load = this.stub().returns(bean);
-//        var model = { presentationModelType: 'SomeClass' };
-//        var onAddedHandler = this.spy();
-//
-//        dolphin.onAdded('SomeOtherClass', onAddedHandler);
-//        onModelStoreChange({ clientPresentationModel: model, eventType: "ADDED" });
-//
-//        sinon.assert.notCalled(onAddedHandler);
-//    }));
-//
-//
-//    it('should call generic onAdded-handler', sinon.test(function() {
-//        var bean = {};
-//        dolphin.classRepository.load = this.stub().returns(bean);
-//        var model = { presentationModelType: 'SomeClass' };
-//        var onAddedHandler = this.spy();
-//
-//        dolphin.onAdded(onAddedHandler);
-//        onModelStoreChange({ clientPresentationModel: model, eventType: "ADDED" });
-//
-//        sinon.assert.calledWith(onAddedHandler, bean);
-//    }));
-//
-//
-//    it('should call onRemoved-handler for class', sinon.test(function() {
-//        var bean = {};
-//        dolphin.classRepository.unload = this.stub().returns(bean);
-//        var model = { presentationModelType: 'SomeClass' };
-//        var onRemovedHandler = this.spy();
-//
-//        dolphin.onRemoved('SomeClass', onRemovedHandler);
-//        onModelStoreChange({ clientPresentationModel: model, eventType: "REMOVED" });
-//
-//        sinon.assert.calledWith(onRemovedHandler, bean);
-//    }));
-//
-//
-//    it('should not call onRemoved-handler for other class', sinon.test(function() {
-//        var bean = {};
-//        dolphin.classRepository.unload = this.stub().returns(bean);
-//        var model = { presentationModelType: 'SomeClass' };
-//        var onRemovedHandler = this.spy();
-//
-//        dolphin.onRemoved('SomeOtherClass', onRemovedHandler);
-//        onModelStoreChange({ clientPresentationModel: model, eventType: "REMOVED" });
-//
-//        sinon.assert.notCalled(onRemovedHandler);
-//    }));
-//
-//
-//    it('should call generic onRemoved-handler', sinon.test(function() {
-//        var bean = {};
-//        dolphin.classRepository.unload = this.stub().returns(bean);
-//        var model = { presentationModelType: 'SomeClass' };
-//        var onRemovedHandler = this.spy();
-//
-//        dolphin.onRemoved(onRemovedHandler);
-//        onModelStoreChange({ clientPresentationModel: model, eventType: "REMOVED" });
-//
-//        sinon.assert.calledWith(onRemovedHandler, bean);
-//    }));
-//});
+describe('Dolphin Event Handling', function() {
+
+    var onModelStoreChange = null;
+    var dolphin = null;
+
+    beforeEach(sinon.test(function() {
+        var clientModelStore = { onModelStoreChange: function(cb) { onModelStoreChange = cb; } };
+        this.stub(opendolphin, "dolphin").returns(
+            {
+                getClientModelStore: function() { return clientModelStore; }
+            }
+        );
+        dolphin = connect("http://localhost");
+        if (typeof onModelStoreChange !== 'function') {
+            throw new Error('Initialisation of opendolphin failed');
+        }
+    }));
+
+    afterEach(function() {
+        dolphin.shutdown();
+    });
 
 
 
-//describe('Dolphin Command', function() {
-//
-//    var dolphin = null;
-//    var opendolphin = null;
-//
-//    beforeEach(function() {
-//        opendolphin = {
-//            getClientModelStore: function() {
-//                return {
-//                    onModelStoreChange: function() {}
-//                };
-//            },
-//            attribute: function() {},
-//            presentationModel: function() {},
-//            send: function() {}
-//        };
-//        var opendolphinStub = {
-//            dolphin: function() { return opendolphin },
-//            Type: { ADDED: "ADDED", REMOVED: "REMOVED" },
-//            '@noCallThru': true
-//        };
-//
-//        var classrepoStub = {
-//            ClassRepository: function () { },
-//            '@noCallThru': true
-//        };
-//
-//        var dolphinjs = proxyquire('../src/dolphin.js', { 'opendolphin': opendolphinStub, './classrepo.js': classrepoStub });
-//
-//        var connect = dolphinjs.connect;
-//        dolphin = connect("http://localhost");
-//    });
-//
-//    afterEach(function() {
-//        dolphin.shutdown();
-//    });
-//
-//
-//
-//    it('should send command without parameters', sinon.test(function() {
-//        this.spy(opendolphin, 'send');
-//
-//        dolphin.send("myCommand");
-//
-//        sinon.assert.calledWith(opendolphin.send, "myCommand");
-//    }));
-//
-//
-//    it('should send command with one named parameter', sinon.test(function() {
-//        dolphin.classRepository.mapParamToDolphin = this.stub().withArgs(42).returns({value: 42, type: 'number'});
-//        var attrFactory = this.stub(opendolphin, 'attribute');
-//        var attr1 = {};
-//        var attr2 = {};
-//        attrFactory.withArgs('x', null, 42, 'VALUE').returns(attr1);
-//        attrFactory.withArgs('x', null, 'number', 'VALUE_TYPE').returns(attr2);
-//        this.spy(opendolphin, 'presentationModel');
-//        this.spy(opendolphin, 'send');
-//
-//        dolphin.send("myCommand", {x: 42});
-//
-//        sinon.assert.calledWith(opendolphin.presentationModel, null, '@@@ DOLPHIN_PARAMETER @@@', attr1, attr2);
-//        sinon.assert.calledWith(opendolphin.send, "myCommand");
-//    }));
-//});
+    it('should call onAdded-handler for class', sinon.test(function() {
+        var bean = {};
+        dolphin.classRepository.load = this.stub().returns(bean);
+        var model = { presentationModelType: 'SomeClass' };
+        var onAddedHandler = this.spy();
+
+        dolphin.onAdded('SomeClass', onAddedHandler);
+        onModelStoreChange({ clientPresentationModel: model, eventType: "ADDED" });
+
+        sinon.assert.calledWith(onAddedHandler, bean);
+    }));
+
+
+    it('should not call onAdded-handler for other class', sinon.test(function() {
+        var bean = {};
+        dolphin.classRepository.load = this.stub().returns(bean);
+        var model = { presentationModelType: 'SomeClass' };
+        var onAddedHandler = this.spy();
+
+        dolphin.onAdded('SomeOtherClass', onAddedHandler);
+        onModelStoreChange({ clientPresentationModel: model, eventType: "ADDED" });
+
+        sinon.assert.notCalled(onAddedHandler);
+    }));
+
+
+    it('should call generic onAdded-handler', sinon.test(function() {
+        var bean = {};
+        dolphin.classRepository.load = this.stub().returns(bean);
+        var model = { presentationModelType: 'SomeClass' };
+        var onAddedHandler = this.spy();
+
+        dolphin.onAdded(onAddedHandler);
+        onModelStoreChange({ clientPresentationModel: model, eventType: "ADDED" });
+
+        sinon.assert.calledWith(onAddedHandler, bean);
+    }));
+
+
+    it('should call onRemoved-handler for class', sinon.test(function() {
+        var bean = {};
+        dolphin.classRepository.unload = this.stub().returns(bean);
+        var model = { presentationModelType: 'SomeClass' };
+        var onRemovedHandler = this.spy();
+
+        dolphin.onRemoved('SomeClass', onRemovedHandler);
+        onModelStoreChange({ clientPresentationModel: model, eventType: "REMOVED" });
+
+        sinon.assert.calledWith(onRemovedHandler, bean);
+    }));
+
+
+    it('should not call onRemoved-handler for other class', sinon.test(function() {
+        var bean = {};
+        dolphin.classRepository.unload = this.stub().returns(bean);
+        var model = { presentationModelType: 'SomeClass' };
+        var onRemovedHandler = this.spy();
+
+        dolphin.onRemoved('SomeOtherClass', onRemovedHandler);
+        onModelStoreChange({ clientPresentationModel: model, eventType: "REMOVED" });
+
+        sinon.assert.notCalled(onRemovedHandler);
+    }));
+
+
+    it('should call generic onRemoved-handler', sinon.test(function() {
+        var bean = {};
+        dolphin.classRepository.unload = this.stub().returns(bean);
+        var model = { presentationModelType: 'SomeClass' };
+        var onRemovedHandler = this.spy();
+
+        dolphin.onRemoved(onRemovedHandler);
+        onModelStoreChange({ clientPresentationModel: model, eventType: "REMOVED" });
+
+        sinon.assert.calledWith(onRemovedHandler, bean);
+    }));
+});
+
+
+
+describe('Dolphin Command', function() {
+
+    var dolphin = null;
+    var clientDolphin = null;
+
+    beforeEach(sinon.test(function() {
+        var clientModelStore = { onModelStoreChange: function(cb) {} };
+        clientDolphin = {
+            getClientModelStore: function() { return clientModelStore; },
+            attribute: function() {},
+            presentationModel: function() {},
+            send: function() {}
+        };
+        this.stub(opendolphin, "dolphin").returns(clientDolphin);
+        dolphin = connect("http://localhost");
+    }));
+
+    afterEach(function() {
+        dolphin.shutdown();
+    });
+
+
+
+    it('should send command without parameters', sinon.test(function() {
+        this.spy(clientDolphin, 'send');
+
+        dolphin.send("myCommand");
+
+        sinon.assert.calledWith(clientDolphin.send, "myCommand");
+    }));
+
+
+    it('should send command with one named parameter', sinon.test(function() {
+        dolphin.classRepository.mapParamToDolphin = this.stub().withArgs(42).returns({value: 42, type: 'number'});
+        var attrFactory = this.stub(clientDolphin, 'attribute');
+        var attr1 = {};
+        var attr2 = {};
+        attrFactory.withArgs('x', null, 42, 'VALUE').returns(attr1);
+        attrFactory.withArgs('x', null, 'number', 'VALUE_TYPE').returns(attr2);
+        this.spy(clientDolphin, 'presentationModel');
+        this.spy(clientDolphin, 'send');
+
+        dolphin.send("myCommand", {x: 42});
+
+        sinon.assert.calledWith(clientDolphin.presentationModel, null, '@@@ DOLPHIN_PARAMETER @@@', attr1, attr2);
+        sinon.assert.calledWith(clientDolphin.send, "myCommand");
+    }));
+});
