6d5f4e7f0b1c2ec45821fd66ccf326239d875ebd
Michael Heinrichs <michael.heinrichs@netopyr.com>
2015-12-08 22:17:08 +0100

    Updated sauce configuration

--- sauce.launchers.js
+++ sauce.launchers.js
@@ -5,13 +5,13 @@ var daily = {
         base: 'SauceLabs',
         browserName: 'chrome',
         platform: 'Windows XP',
-        version: '43.0'
+        version: '46.0'
     },
     sl_winXP_firefox: {
         base: 'SauceLabs',
         browserName: 'firefox',
         platform: 'Windows XP',
-        version: '39.0'
+        version: '42.0'
     },
 
     sl_win7_ie9: {
@@ -36,13 +36,13 @@ var daily = {
         base: 'SauceLabs',
         browserName: 'chrome',
         platform: 'Windows 7',
-        version: '43.0'
+        version: '46.0'
     },
     sl_win7_firefox: {
         base: 'SauceLabs',
         browserName: 'firefox',
         platform: 'Windows 7',
-        version: '39.0'
+        version: '42.0'
     },
 
     sl_win8_0_ie10: {
@@ -55,13 +55,13 @@ var daily = {
         base: 'SauceLabs',
         browserName: 'chrome',
         platform: 'Windows 8',
-        version: '43.0'
+        version: '46.0'
     },
     sl_win8_0_firefox: {
         base: 'SauceLabs',
         browserName: 'firefox',
         platform: 'Windows 8',
-        version: '39.0'
+        version: '42.0'
     },
 
     sl_win8_1_ie11: {
@@ -74,38 +74,45 @@ var daily = {
         base: 'SauceLabs',
         browserName: 'chrome',
         platform: 'Windows 8.1',
-        version: '43.0'
+        version: '46.0'
     },
     sl_win8_1_firefox: {
         base: 'SauceLabs',
         browserName: 'firefox',
         platform: 'Windows 8.1',
-        version: '39.0'
+        version: '42.0'
     },
 
-    sl_ipad_8_2: {
+    sl_mac11_chrome: {
         base: 'SauceLabs',
-        browserName: 'Safari',
-        appiumVersion: '1.4.7',
-        deviceName: 'iPad Simulator',
-        deviceOrientation: 'portrait',
-        platformName: 'iOS',
-        platformVersion: '8.2'
-    }
-};
+        browserName: 'chrome',
+        platform: 'OS X 10.11',
+        version: '46.0'
+    },
+    sl_mac11_firefox: {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'OS X 10.11',
+        version: '42.0'
+    },
+    sl_mac11_safari: {
+        base: 'SauceLabs',
+        browserName: 'safari',
+        platform: 'OS X 10.11',
+        version: '9.0'
+    },
 
-var weekly = {
     sl_mac10_chrome: {
         base: 'SauceLabs',
         browserName: 'chrome',
-        platform: 'OSX 10.10',
-        version: '43.0'
+        platform: 'OS X 10.10',
+        version: '46.0'
     },
     sl_mac10_firefox: {
         base: 'SauceLabs',
         browserName: 'firefox',
-        platform: 'OSX 10.10',
-        version: '39.0'
+        platform: 'OS X 10.10',
+        version: '42.0'
     },
     sl_mac10_safari: {
         base: 'SauceLabs',
@@ -118,13 +125,13 @@ var weekly = {
         base: 'SauceLabs',
         browserName: 'chrome',
         platform: 'Linux',
-        version: '43.0'
+        version: '46.0'
     },
     sl_linux_firefox: {
         base: 'SauceLabs',
         browserName: 'firefox',
         platform: 'Linux',
-        version: '39.0'
+        version: '42.0'
     },
     sl_linux_opera: {
         base: 'SauceLabs',
@@ -133,49 +140,48 @@ var weekly = {
         version: '12.15'
     },
 
-    sl_ipad_8_1: {
+    sl_ipad_8_4: {
         base: 'SauceLabs',
         browserName: 'Safari',
-        appiumVersion: '1.4.7',
+        appiumVersion: '1.4.16',
         deviceName: 'iPad Simulator',
         deviceOrientation: 'portrait',
         platformName: 'iOS',
-        platformVersion: '8.1'
+        platformVersion: '8.4'
     },
-    sl_ipad_8_0: {
+    sl_ipad_9_1: {
         base: 'SauceLabs',
         browserName: 'Safari',
-        appiumVersion: '1.4.7',
+        appiumVersion: '1.4.16',
         deviceName: 'iPad Simulator',
         deviceOrientation: 'portrait',
         platformName: 'iOS',
-        platformVersion: '8.0'
+        platformVersion: '9.1'
     },
 
     sl_android_5_1: {
         base: 'SauceLabs',
-        browserName: 'Android',
-        platform: 'Linux',
-        version: '5.1',
-        deviceName: 'Android Emulator',
-        deviceOrientation: 'portrait'
+        browserName: 'Browser',
+        appiumVersion: '1.4.16',
+        deviceName: 'Android Simulator',
+        deviceOrientation: 'portrait',
+        platformName: 'Android',
+        platformVersion: '5.1'
     }
 };
 
-
-
-var manual = {
+var weekly = {
     sl_mac9_chrome: {
         base: 'SauceLabs',
         browserName: 'chrome',
-        platform: 'OSX 10.9',
-        version: '43.0'
+        platform: 'OS X 10.9',
+        version: '46.0'
     },
     sl_mac9_firefox: {
         base: 'SauceLabs',
         browserName: 'firefox',
-        platform: 'OSX 10.9',
-        version: '39.0'
+        platform: 'OS X 10.9',
+        version: '42.0'
     },
     sl_mac9_safari: {
         base: 'SauceLabs',
@@ -184,40 +190,49 @@ var manual = {
         version: '7.0'
     },
 
-    sl_ipad_7_1: {
+    sl_ipad_8_3: {
         base: 'SauceLabs',
         browserName: 'Safari',
-        appiumVersion: '1.4.7',
+        appiumVersion: '1.4.16',
         deviceName: 'iPad Simulator',
         deviceOrientation: 'portrait',
         platformName: 'iOS',
-        platformVersion: '7.1'
-    },
-    sl_ipad_7_0: {
+        platformVersion: '8.3'
+    }
+
+};
+
+
+
+var manual = {
+    sl_ipad_9_2: {
         base: 'SauceLabs',
         browserName: 'Safari',
-        appiumVersion: '1.4.7',
+        appiumVersion: '1.4.16',
         deviceName: 'iPad Simulator',
         deviceOrientation: 'portrait',
         platformName: 'iOS',
-        platformVersion: '7.0'
+        platformVersion: '9.2'
     },
 
     sl_android_5_0: {
         base: 'SauceLabs',
-        browserName: 'Android',
-        platform: 'Linux',
-        version: '5.0',
-        deviceName: 'Android Emulator',
-        deviceOrientation: 'portrait'
+        browserName: 'Browser',
+        appiumVersion: '1.4.16',
+        deviceName: 'Android Simulator',
+        deviceOrientation: 'portrait',
+        platformName: 'Android',
+        platformVersion: '5.0'
     },
+
     sl_android_4_4: {
         base: 'SauceLabs',
-        browserName: 'Android',
-        platform: 'Linux',
-        version: '4.4',
-        deviceName: 'Android Emulator',
-        deviceOrientation: 'portrait'
+        browserName: 'Browser',
+        appiumVersion: '1.4.16',
+        deviceName: 'Android Simulator',
+        deviceOrientation: 'portrait',
+        platformName: 'Android',
+        platformVersion: '4.4'
     }
 };
 
