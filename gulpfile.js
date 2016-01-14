0f3f7a98fe8764759029283fa5220afc27c92514
Michael Heinrichs <michael.heinrichs@netopyr.com>
2016-01-14 16:26:29 +0100

    Set version 0.7.3-SNAPSHOT

--- gulpfile.js
+++ gulpfile.js
@@ -124,7 +124,7 @@ gulp.task('sonar', ['ci'], function () {
             },
             projectKey: 'dolphin-js',
             projectName: 'Dolphin Platform JS',
-            projectVersion: '0.7.2',
+            projectVersion: '0.7.3-SNAPSHOT',
             sources: 'src',
             language: 'js',
             sourceEncoding: 'UTF-8',
