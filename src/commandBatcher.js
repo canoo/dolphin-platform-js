60300b1ed7f085af75026068013bc87d3d0d9330
Hendrik Ebbers <hendrik.ebbers@web.de>
2017-08-22 08:16:08 +0200

    simple commnd batcher

--- src/commandBatcher.js
+++ src/commandBatcher.js
@@ -1,46 +1,20 @@
-import ValueChangedCommand from './commands/impl/valueChangedCommand'
-
-export class NoCommandBatcher {
-    batch(queue) {
-        return [queue.shift()];
-    }
-}
-
-export class BlindCommandBatcher {
-    /** folding: whether we should try folding ValueChangedCommands */
+export default class BlindCommandBatcher {
     constructor(folding = true, maxBatchSize = 50) {
         this.folding = folding;
         this.maxBatchSize = maxBatchSize;
     }
     batch(queue) {
         let batch = [];
-        const n = Math.min(queue.length, this.maxBatchSize);
-        for (let counter = 0; counter < n; counter++) {
-            const candidate = queue.shift();
-            if (this.folding && candidate.command instanceof ValueChangedCommand && (!candidate.handler)) {
-                const canCmd = candidate.command;
-                if (batch.length > 0 && batch[batch.length - 1].command instanceof ValueChangedCommand) {
-                    const batchCmd = batch[batch.length - 1].command;
-                    if (canCmd.attributeId == batchCmd.attributeId) {
-                        batchCmd.newValue = canCmd.newValue;
-                    }
-                    else {
-                        batch.push(candidate); // we cannot merge, so batch the candidate
-                    }
-                }
-                else {
-                    batch.push(candidate); // we cannot merge, so batch the candidate
-                }
-            }
-            else {
-                batch.push(candidate);
-            }
-            if (candidate.handler ||
-                (candidate.command['className'] == "org.opendolphin.core.comm.EmptyNotification") // or unknown client side effect
-            ) {
-                break; // leave the loop
+        let batchLenght = 0;
+        while(queue[batchLenght] && batchLenght <= this.maxBatchSize) {
+            const element = queue[batchLenght];
+            batch.push(element);
+            batchLenght++;
+            if(element.handler) {
+                break;
             }
         }
+        queue.splice(0, batchLenght);
         return batch;
     }
 }
