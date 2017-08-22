2f9765bf848d989f353f3024ee86d163c63aeb23
Hendrik Ebbers <hendrik.ebbers@web.de>
2017-08-22 08:33:42 +0200

    Merge value change

--- src/commandBatcher.js
+++ src/commandBatcher.js
@@ -1,3 +1,6 @@
+import {VALUE_CHANGED_COMMAND_ID, PRESENTATION_MODEL_DELETED_COMMAND_ID} from '../commandConstants';
+
+
 export default class BlindCommandBatcher {
     constructor(folding = true, maxBatchSize = 50) {
         this.folding = folding;
@@ -8,8 +11,22 @@ export default class BlindCommandBatcher {
         let batchLenght = 0;
         while(queue[batchLenght] && batchLenght <= this.maxBatchSize) {
             const element = queue[batchLenght];
-            batch.push(element);
             batchLenght++;
+            if(this.folding) {
+                if(element.command.id == VALUE_CHANGED_COMMAND_ID ||
+                    batch.length > 0 ||
+                    batch[batch.length - 1].command.id == VALUE_CHANGED_COMMAND_ID ||
+                    element.command.attributeId == batch[batch.length - 1].command.attributeId) {
+                    //merge ValueChange for same value
+                    batch[batch.length - 1].command.newValue = element.command.newValue;
+                } else if(element.command.id == PRESENTATION_MODEL_DELETED_COMMAND_ID) {
+                    //We do not need it...
+                } else {
+                    batch.push(element);
+                }
+            } else {
+                batch.push(element);
+            }
             if(element.handler) {
                 break;
             }
