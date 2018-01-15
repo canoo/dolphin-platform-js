9aa1accce62ec64b55266dba31ccfc968f4e3e48
giftkugel <github@giftkugel.de>
2018-01-15 08:40:03 +0100

    Add webpack, add NPM scripts, build with webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+    entry: {
+        'dolphin-platform.bundle': './src/clientContextFactory.js',
+        'dolphin-platform.bundle.min': './src/clientContextFactory.js'
+    },
+    devtool: 'source-map',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        library: 'dolphin',
+        libraryTarget: 'umd',
+        //umdNamedDefine: true
+    },
+    plugins: [
+        new UglifyJsPlugin({
+            include: /\.min\.js$/
+        })
+    ]
+};
