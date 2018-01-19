3fc9c07be424dbe50dc6b6ed6d3ed2c12e5ffad0
giftkugel <github@giftkugel.de>
2018-01-19 12:42:16 +0100

    Updated build.

--- dist/dolphin-platform.js
+++ dist/dolphin-platform.js
@@ -6264,13 +6264,12 @@ class PlatformHttpTransmitter {
                 }
             }
 
+            this.logger.trace('_send', commands, encodedCommands);
             if (this.failed_attempt > this.maxRetry) {
                 setTimeout(function() {
-                    this.logger.trace('_send', commands, encodedCommands);
                     http.send(encodedCommands);
                 }, this.timeout);
             }else{
-                this.logger.trace('_send', commands, encodedCommands);
                 http.send(encodedCommands);
             }
 
