cb16b2e971ce9466ff24ea6686ed01dbf4d037fd
giftkugel <github@giftkugel.de>
2018-02-23 19:23:28 +0100

    Improved HttpException

--- src/http/httpException.js
+++ src/http/httpException.js
@@ -1,11 +1,21 @@
 class HttpException {
 
-    constructor(message) {
+    constructor(message, status, timedout) {
         this.message = message;
+        this.status = status || 0;
+        this.timedout = timedout || false;
     }
 
     getMessage() {
         return this.message;
+    }
+
+    getStatus() {
+        return this.status;
+    }
+
+    isTimedout() {
+        return this.timedout;
     }
 
 }
