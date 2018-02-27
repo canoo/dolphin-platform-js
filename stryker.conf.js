5e2587f342ebb0724ee29053f3adac22ab7487ae
giftkugel <github@giftkugel.de>
2018-02-27 17:41:23 +0100

    Added mutation testing

---
+++ stryker.conf.js
@@ -0,0 +1,23 @@
+module.exports = function(config) {
+  config.set({
+    files: [
+      {
+        pattern: "src/**/*.js",
+        mutated: true,
+        included: false
+      },
+      "test/**/*.js"
+    ],
+    testRunner: "mocha",
+    mochaOptions: {
+      // Optional mocha options
+      require: [ 'babel-register' ,'jsdom-global/register']
+    },
+    mutator: "javascript",
+    transpilers: ["babel"],
+    reporter: ["html", "clear-text", "progress"],
+    testFramework: "mocha",
+    coverageAnalysis: "off",
+    babelrcFile: ".babelrc"
+  });
+};
