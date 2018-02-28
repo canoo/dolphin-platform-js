cd0b7134114784fb919ad0d8060344a2fc892062
giftkugel <github@giftkugel.de>
2018-02-28 14:05:21 +0100

    Removed unnecessary Log messages

--- src/platform/platformClient.js
+++ src/platform/platformClient.js
@@ -43,8 +43,7 @@ PlatformClient.getAllServiceTypes = function() {
 
 PlatformClient.registerServiceProvider = function(serviceProvider) {
     if (serviceProvider === null || typeof serviceProvider === 'undefined') {
-        PlatformClient.LOGGER.error('Cannot register service provider');
-        throw new Error('Cannot register service provider');
+        throw new Error('Cannot register empty service provider');
     }
     
     if (typeof serviceProvider.getName === 'function' && typeof serviceProvider.getService === 'function') {
@@ -53,11 +52,9 @@ PlatformClient.registerServiceProvider = function(serviceProvider) {
             PlatformClient.serviceProviders.set(serviceProvider.getName(), serviceProvider);
             PlatformClient.LOGGER.debug('Service provider registered with name', serviceProvider.getName());
         } else {
-            PlatformClient.LOGGER.error('Cannot register another service provider. Name already in use.', serviceProvider.getName());
             throw new Error('Cannot register another service provider. Name already in use.');
         }
     } else {
-        PlatformClient.LOGGER.error('Cannot register service provider without getName() and getService() methods');
         throw new Error('Cannot register service provider without getName() and getService() methods');
     }
 };
