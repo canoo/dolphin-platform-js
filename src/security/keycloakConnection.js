9cd4aeb7d604e56787b9840f6d3c8db099c15b5c
giftkugel <github@giftkugel.de>
2018-05-25 14:07:25 +0200

    Real name will now be set for proxy based connection

--- src/security/keycloakConnection.js
+++ src/security/keycloakConnection.js
@@ -13,19 +13,20 @@ class KeycloakConnection {
 
         const httpRequest = new XMLHttpRequest();
         httpRequest.open(HTTP.METHOD.POST, authEndpoint + '/auth/realms/' + realmName + '/protocol/openid-connect/token', true);
-        httpRequest.setRequestHeader(HTTP.HEADER_NAME.CONTENT_TYPE,  HTTP.CONTENT_TYPE.APPLICATION_X_WWW_FORM_URLENCODED);
+        httpRequest.setRequestHeader(HTTP.HEADER_NAME.CONTENT_TYPE, HTTP.CONTENT_TYPE.APPLICATION_X_WWW_FORM_URLENCODED);
         httpRequest.responseType = RESPONSE_TYPE.JSON;
 
         return httpRequest;
     }
 
-    createServerProxyConnection(authEndpoint) {
+    createServerProxyConnection(authEndpoint, realmName) {
         checkMethod('createServerProxyConnection');
         checkParam(authEndpoint, 'authEndpoint');
 
         const httpRequest = new XMLHttpRequest();
         httpRequest.open(HTTP.METHOD.POST, authEndpoint, true);
-        httpRequest.setRequestHeader(HTTP.HEADER_NAME.CONTENT_TYPE,  HTTP.CONTENT_TYPE.TEXT_PLAIN);
+        httpRequest.setRequestHeader(HTTP.HEADER_NAME.CONTENT_TYPE, HTTP.CONTENT_TYPE.TEXT_PLAIN);
+        httpRequest.setRequestHeader(HTTP.HEADER_NAME.X_PLATFORM_SECURITY_REALM, realmName);
         httpRequest.responseType = RESPONSE_TYPE.JSON;
 
         return httpRequest;
